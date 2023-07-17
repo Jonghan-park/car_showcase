586499dbd36d98cb56184f5eebbf5157c00599e6
Jonghan Park <jonghan.park@edu.sait.ca>
2023-07-17 16:56:01 -0700

    Fix overlay image and layout of hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,12 +15,12 @@ const Hero = () => {
         </h1>
         <p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process.</p>
         <CustomButton title="Explore Cars" btnType='button' containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
+          </div>
         <div className="hero__image-container">
           <div className="hero__image">
             <Image src="/hero.png" alt='hero' fill className='object-contain' />
-            <div className="hero__image-overlay"></div>
-          </div>
         </div>
+            <div className="hero__image-overlay"></div>
       </div>
     </div>
   )
