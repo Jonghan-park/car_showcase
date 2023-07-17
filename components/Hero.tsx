b33ff9258e56251f8eeede645238c28a37628898
Jonghan Park <jonghan.park@edu.sait.ca>
2023-07-17 16:28:06 -0700

    Update Navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
           Find, book, or rent a car -- quickly and easily!
         </h1>
         <p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process.</p>
-        <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
+        <CustomButton title="Explore Cars" btnType='button' containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
         <div className="hero__image-container">
           <div className="hero__image">
             <Image src="/hero.png" alt='hero' fill className='object-contain' />
