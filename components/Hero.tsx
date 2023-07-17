05051d5445b59ca0ac4ef3c36973bdc9e6cf6cd4
Jonghan Park <jonghan.park@edu.sait.ca>
2023-07-17 16:19:07 -0700

    Update so many pictures, and make Navbar and Footer components

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,6 +15,12 @@ const Hero = () => {
         </h1>
         <p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process.</p>
         <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image src="/hero.png" alt='hero' fill className='object-contain' />
+            <div className="hero__image-overlay"></div>
+          </div>
+        </div>
       </div>
     </div>
   )
